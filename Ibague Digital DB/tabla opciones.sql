320a544a3efc63c4acc68c701c746d6f9595b518
Cristian Perez <111152484+CristianAndresPB@users.noreply.github.com>
2025-09-24 21:40:36 -0500

    Delete Ibague Digital DB/tabla opciones.sql
    borrar

--- Ibague Digital DB/tabla opciones.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE opciones (
-    id_opcion INT AUTO_INCREMENT PRIMARY KEY,
-    id_pregunta INT NOT NULL,
-    texto_opcion VARCHAR(100) NOT NULL,
-    FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta) ON DELETE CASCADE
-);
